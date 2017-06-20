87c6aa68526d41dc541192695cfea08964c8aea1
mervyn.chung <mervyn.chung@foxmail.com>
2017-06-21 01:35:45 +0800

    0620#02

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 //app.js
+//#000000 主标题字色
+//#888888 次要信息字色
+//#bfbfbf 暗提示色
+//#f4333c 错误、警示色
+//#ff5b05 重点提醒色
+//#f5f5f9 app底色
+//#3FBE69 app主题色
 App({
   onLaunch: function () {
     //调用API从本地缓存中获取数据
-    
+
   },
-  globalData:{},
-  host:"http://120.76.241.230:8090"
+  globalData: {},
+  host: "http://120.76.241.230:8090"
 })

ffe195ed1bee18c77ccc9d885d1006fa7a19ed5b
neko <mervyn.chung@foxmail.com>
2017-06-22 15:10:14 +0800

    0622#02

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //app.js
-//#000000 主标题字色
+//#000000 主要内容、标题字色
 //#888888 次要信息字色
+//#353535 大段的说明且属于主要内容
 //#bfbfbf 暗提示色
 //#f4333c 错误、警示色
 //#ff5b05 重点提醒色
@@ -8,9 +9,10 @@
 //#3FBE69 app主题色
 App({
   onLaunch: function () {
+    //wx.setStorageSync('token','admin')
     //调用API从本地缓存中获取数据
     const token = wx.getStorageSync('token');
-    if(!token){
+    if(!!token){
       wx.redirectTo({
         url: 'pages/login/login',
       })

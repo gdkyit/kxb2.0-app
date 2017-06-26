38bce7dc60063aa1eb36836e1a3bed7b72f5d477
neko <mervyn.chung@foxmail.com>
2017-06-26 18:28:09 +0800

    0626#04

--- pages/dtxx/dtxx.js
+++ pages/dtxx/dtxx.js
@@ -5,63 +5,54 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    questions:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log(options)
-  
+    let tkId = options.tkId
+    getList(tkId);
   },
-
   /**
-   * 生命周期函数--监听页面初次渲染完成
+   * 获取随机题目
    */
-  onReady: function () {
-  
-  },
+  getList: function (tkid) {
+    const token = wx.getStorageSync('token');
+    wx.request({
+      url: app.host + '/api/tk',
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: {
+        'content-type': 'application/json',
+        'x-auth-token': token
+      }, // 设置请求的 header
+      data:{parentId:tkid},
+      success: (res) => {
+        if (res.statusCode == '200') {
+          this.setData({
+            questions: res.data.data,
+          })
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+        } else {
+          wx.showToast({
+            title: res.data.error,
+            image: '/resource/img/error.png',
+            duration: 3000
+          });
+        }
+      },
+      fail: function () {
+        wx.showToast({
+          title: '网络访问故障',
+          image: '/resource/img/error.png',
+          duration: 3000
+        });
+      },
+      complete: function () {
+        // complete
+      }
+    })
   }
+
 })

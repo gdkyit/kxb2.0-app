2bb0b6d7603f5d06fc4774ba0b786c038a2e0ac1
mervyn.chung <mervyn.chung@foxmail.com>
2017-07-11 00:14:19 +0800

    0710#02

--- app.js
+++ app.js
@@ -1,55 +1,52 @@
 //app.js
-//#000000 主要内容、标题字色
-//#888888 次要信息字色
-//#353535 大段的说明且属于主要内容
-//#bfbfbf 暗提示色
-//#f4333c 错误、警示色
-//#ff5b05 重点提醒色
-//#f5f5f9 app底色
-//#3FBE69 app主题色
+
 App({
   onLaunch: function () {
     //调用API从本地缓存中获取数据
   },
   onShow: function () {
-    const token = wx.getStorageSync('token');
-    if (!this.launching) {
-      wx.request({
-        url: this.host + '/api/messages/groupchange',
-        method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-        header: {
-          'content-type': 'application/json',
-          'x-auth-token': token
-        }, // 设置请求的 header
-        success: (res) => {
-          if (res.statusCode == '200') {
-           wx.showModal({
-              title: 'join group',
-              content: 'join group',
-              showCancel:false,
-              mask:true
-            });
-
-          } else {
-            wx.showToast({
-              title: res.data.error,
-              image: '/resource/img/error.png',
-              duration: 3000
-            });
-          }
-        },
-        fail: function () {
-          wx.showToast({
-            title: '网络访问故障',
-            image: '/resource/img/error.png',
-            duration: 3000
-          });
-        },
-        complete: function () {
-        }
-      })
+    let pages = getCurrentPages();
+    if (pages.length > 0 && pages[pages.length - 1].route != 'pages/index/index') {
+      this.getSysMsg();
     }
 
+  },
+  getSysMsg() {
+    const token = wx.getStorageSync('token');
+    wx.request({
+      url: this.host + '/api/messages/groupchange',
+      method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      header: {
+        'content-type': 'application/json',
+        'x-auth-token': token
+      }, // 设置请求的 header
+      success: (res) => {
+        if (res.statusCode == '200') {
+          if (res.data.code == '200') {
+            wx.showModal({
+              title: 'join group',
+              content: 'join group',
+              showCancel: false,
+              mask: true
+            });
+          }
+        } else if (res.statusCode == '401') {
+          wx.removeStorageSync('token')
+          wx.reLaunch({
+            url: '../login/login',
+          })
+        }
+      },
+      fail: function () {
+        wx.showToast({
+          title: '网络访问故障',
+          image: '/resource/img/error.png',
+          duration: 3000
+        });
+      },
+      complete: function () {
+      }
+    })
   },
   globalData: {},
   host: "http://120.76.241.230:8090",
--- pages/index/index.js
+++ pages/index/index.js
@@ -4,7 +4,7 @@ Page({
 
     },
     onLoad: function (option) {
-        app.launching = true;
+
         wx.getStorage({ //获取token
             key: 'token',
             success: function (res) {
@@ -12,8 +12,6 @@ Page({
                     wx.reLaunch({
                         url: '../kaoshi/kaoshi'
                     })
-                    app.launching = false;
-                    app.onShow();
                 },3000)
             },
             fail: err => { //获取token失败

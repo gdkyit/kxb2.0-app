c60a81423ef6484702491a8eb3e391c7a7aab8f2
neko <mervyn.chung@foxmail.com>
2017-07-11 16:04:52 +0800

    0711#02

--- pages/ranking/ranking.js
+++ pages/ranking/ranking.js
@@ -78,15 +78,31 @@ Page({
       }, // 设置请求的 header
       success: (res) => {
         if (res.statusCode == '200') {
-          res.data.data.userScoreRank.score = res.data.data.userScoreRank.score.toFixed(2)
-          for (let i = 0; i < res.data.data.scoreRank.length; i++) {
-            let user = res.data.data.scoreRank[i];
-            user.score = user.score.toFixed(2);
+          if (res.data.code == '200') {
+            res.data.data.userScoreRank.score = res.data.data.userScoreRank.score.toFixed(2)
+            for (let i = 0; i < res.data.data.scoreRank.length; i++) {
+              let user = res.data.data.scoreRank[i];
+              user.score = user.score.toFixed(2);
+            }
+            this.setData({
+              userScoreRank: res.data.data.userScoreRank,
+              scoreRank: res.data.data.scoreRank
+            })
+          }else if(res.data.error == '没设置默认积分榜'){
+            wx.showModal({
+              title:'未设置默认积分榜',
+              content:'请先设置默认积分榜',
+              mask:true,
+              showCancel:false,
+              confirmText:'立即设置',
+              success:e=>{
+                wx.redirectTo({
+                  url: '../user_rankSetting/user_rankSetting',
+                })
+              }
+            })
           }
-          this.setData({
-            userScoreRank: res.data.data.userScoreRank,
-            scoreRank: res.data.data.scoreRank
-          })
+
 
         } else {
           wx.showToast({
@@ -156,7 +172,9 @@ Page({
     wx.request({
       url: app.host + '/api/qzb',
       method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-      data: { groupId: gid },
+      data: {
+        groupId: gid
+      },
       header: {
         'content-type': 'application/json',
         'x-auth-token': token
@@ -241,7 +259,9 @@ Page({
     wx.request({
       url: app.host + '/api/ksb',
       method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-      data: { examId: eid },
+      data: {
+        examId: eid
+      },
       header: {
         'content-type': 'application/json',
         'x-auth-token': token
@@ -326,7 +346,9 @@ Page({
     wx.request({
       url: app.host + '/api/flpm',
       method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
-      data: { flId: eid },
+      data: {
+        flId: eid
+      },
       header: {
         'content-type': 'application/json',
         'x-auth-token': token
@@ -366,17 +388,23 @@ Page({
   bindUserGroup(e) {
     let gid = e.currentTarget.dataset.gid
     this.getQzb(gid);
-    this.setData({ view: '12' })
+    this.setData({
+      view: '12'
+    })
   },
   bindUserExam(e) {
     let eid = e.currentTarget.dataset.eid
     this.getKsb(eid);
-    this.setData({ view: '22' })
+    this.setData({
+      view: '22'
+    })
   },
   bindUserFlpm(e) {
     let eid = e.currentTarget.dataset.eid
     this.getFlpm(eid);
-    this.setData({ view: '32' })
+    this.setData({
+      view: '32'
+    })
   },
   bindPhbChange: function (e) {
     let phb = e.detail.value
@@ -385,22 +413,32 @@ Page({
     })
     if (phb == 0) {
       this.getJfb();
-      this.setData({ view: '0' })
+      this.setData({
+        view: '0'
+      })
     } else if (phb == 1) {
       this.getQzbList();
-      this.setData({ view: '11' })
+      this.setData({
+        view: '11'
+      })
     } else if (phb == 2) {
       this.getKsbList();
-      this.setData({ view: '21' })
+      this.setData({
+        view: '21'
+      })
     } else if (phb == 3) {
       this.getFlpmList();
-      this.setData({ view: '31' })
+      this.setData({
+        view: '31'
+      })
     }
   },
-  bindback(e){
+  bindback(e) {
     let pv = e.currentTarget.dataset.pv;
-    if(!!pv){
-      this.setData({view:pv})
+    if (!!pv) {
+      this.setData({
+        view: pv
+      })
     }
   }
 

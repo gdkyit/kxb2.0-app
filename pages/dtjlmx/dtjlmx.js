a5d99f3f51966322e707d6c786b3dcc79a020573
neko <mervyn.chung@foxmail.com>
2017-07-13 15:11:21 +0800

    0713#02

--- pages/dtjlmx/dtjlmx.js
+++ pages/dtjlmx/dtjlmx.js
@@ -1,36 +1,31 @@
 var common = require ('../../lib/common.js')
-// dtjl.js
+// dtjlmx.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    list: []
+    list:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    this.getList();
-  },
-
-  /**
-   * 获取答题记录列表
-   */
-  getList: function () {
     const app = getApp();
     const token = wx.getStorageSync('token');
+    wx.showNavigationBarLoading();
     wx.request({
-      url: app.host + '/api/dtxxRecordList',
+      url: app.host + '/api/dtxxRecordDetail',
       method: 'GET', // OPTIONS, GET, HEAD, POST, PUT, DELETE, TRACE, CONNECT
+      data:{day:options.day},
       header: {
         'content-type': 'application/json',
         'x-auth-token': token
       }, // 设置请求的 header
       success: (res) => {
-        if (res.statusCode == '200') {
+        if (res.statusCode == '200' && res.data.code == '200') {
           this.setData({
             list: res.data.data,
           })
@@ -39,7 +34,7 @@ Page({
           wx.showToast({
             title: res.data.error,
             image: '/resource/img/error.png',
-            duration: 3000
+            duration: 2000
           });
         }
       },
@@ -47,18 +42,19 @@ Page({
         wx.showToast({
           title: '网络访问故障',
           image: '/resource/img/error.png',
-          duration: 3000
+          duration: 2000
         });
       },
       complete: function () {
-        // complete
+        wx.hideNavigationBarLoading();
       }
     })
   },
+
   bindtouchstart(e) {
-    common.bindtouchstart(e, this)
+    common.bindtouchstart(e,this)
   },
   bindtouchend(e) {
-    common.bindtouchend(e, this);
+   common.bindtouchend(e,this);
   }
 })
--- pages/xdjlmx/xdjlmx.js
+++ pages/xdjlmx/xdjlmx.js
@@ -1,3 +1,4 @@
+var common = require('../../lib/common.js')
 // xdjlmx.js
 var WxParse = require('../../lib/wxParse/wxParse.js');
 Page({
@@ -30,32 +31,10 @@ Page({
       title: title
     })
   },
-
-  /**
-   * 水平滑动返回
-   */
   bindtouchstart(e) {
-    this.setData({
-      clientx: e.changedTouches[0].clientX,
-      clienty: e.changedTouches[0].clientY,
-      timeStamp: e.timeStamp
-    })
+    common.bindtouchstart(e, this)
   },
   bindtouchend(e) {
-    let lastx = e.changedTouches[0].clientX;
-    let lasty = e.changedTouches[0].clientY;
-    let lastTimeStamp = e.timeStamp;
-
-    //滑动水平距离超过100个像素，水平夹角不超过15°（使用tan值做条件判断），判定为滑动翻页
-    // 水平滑动距离判定条件值，可按需调整，当前取值100像素
-    const conDisX = 100; 
-    //滑动水平夹角判定条件值，可按需调整,当前取值15度
-    const conTanY = Math.tan(18 * Math.PI/180); 
-
-    let distanceX = lastx - this.data.clientx;
-    let tan = Math.abs((lasty - this.data.clienty) / distanceX)
-    if (tan < conTanY && distanceX > conDisX) {
-      wx.navigateBack();
-    }
+    common.bindtouchend(e, this);
   }
 })
